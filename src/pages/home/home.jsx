4e6c30c4a44212047f3f3490310087c4bd461fbf
Jeremy <jcbball55@gmail.com>
2024-01-08 10:11:46 -0500

    Added basic homescreen

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -4,11 +4,51 @@ import styles from "./home.module.css";
 import Header from "../../components/header/header";
 import Footer from "../../components/footer/footer";
 
+import hikingZahid from "../../images/hikingZahidTwo.jpg";
+import runningZahid from "../../images/runningZahid.jpg";
+
 const HomePage = () => {
     return (
         <div className={styles.container}>
             <Header />
-            <h1>Home</h1>
+            <div className={styles.bodyContainer}>
+                <div className={styles.header}>
+                    <img
+                        src={hikingZahid}
+                        alt="A picture of zahid hiking in the mountains"
+                        srcset=""
+                        className={styles.headerImage}
+                    />
+
+                    <div className={styles.headerText}>
+                        <h2 className={styles.level}>LEVEL UP WITH</h2>
+                        <h1 className={styles.nutrition}>NUTRITION</h1>
+                        <button className={styles.getStarted}>
+                            Get Started!
+                        </button>
+                    </div>
+                </div>
+
+                <div className={styles.about}>
+                    <img
+                        src={runningZahid}
+                        alt="A picture of zahid running"
+                        srcset=""
+                        className={styles.zahidRunning}
+                    />
+                    <div className={styles.aboutText}>
+                        <h1>Who am I?</h1>
+                        <p>
+                            I'm Zahid, a registered dietitian who can help you
+                            reach your fitness goals through the power of
+                            nutrition.
+                        </p>
+                        <button className={styles.aboutButton}>
+                            Learn More!
+                        </button>
+                    </div>
+                </div>
+            </div>
             <Footer />
         </div>
     );
